ac7f4f161ef9523ea34697ce86f229a6b9fc00f8
un-earthly <vijayalamin@gmail.com>
2022-07-19 02:29:45 -0700

    home skeleton

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -3,6 +3,5 @@ import { useRouter } from 'next/router'
 export default function ProductDetails() {
     const router = useRouter()
     const { id } = router.query
-
     return <p>ProductDetails for: {id}</p>
 }
