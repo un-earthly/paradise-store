cbb1d54caa6d6aad8417757da501b0fd21ad22c0
un-earthly <vijayalamin@gmail.com>
2022-07-19 11:08:48 -0700

    about page design

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -5,7 +5,7 @@ export default function HomeProducts() {
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-wrap -m-4">
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-white">
+                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full bg-base-300 rounded">
                         <a className="block relative h-48 rounded overflow-hidden">
                             <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
                         </a>
